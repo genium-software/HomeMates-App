cbb0ccb6a222ec3efbc0fd3fb0f97e1abb3eb37c
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2019-11-12 17:16:14 +1100

    Add titlebar function for navigation stacks

---
+++ homematesapp/utils/titlebar.js
@@ -0,0 +1,14 @@
+export default Titlebar = title => {
+  return {
+    title: title,
+    headerStyle: {
+      backgroundColor: "#212121"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      fontSize: 20,
+      fontWeight: "500",
+      textAlign: "center"
+    }
+  };
+};
