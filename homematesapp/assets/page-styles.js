741863daba83a811099895bc2e82a9ba684699b1
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2019-11-12 17:16:33 +1100

    Separate styling page

---
+++ homematesapp/assets/page-styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default pageStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  text: {
+    marginVertical: 16
+  }
+});
